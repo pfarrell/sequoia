578bcb5aa4dc5f0aa686e8d6e3d33493330014a1
Patrick Farrell <pfarrell@prosper.com>
2014-11-11 15:33:48 -0800

    formatting

--- init.rb
+++ init.rb
@@ -62,7 +62,7 @@ p.save
 
 p=Party.find_or_create(title:'Scotch and Cheese Party')
 p.donor='Gudiksen/Okamoto Families (Annalise/Lucy)'
-p.description='<p>Join us for an evening of fine whisky tasting and cheese pairing.  We will taste selections from all over Scotland - Lowlands, Highlands, Speyside, and Islay malts.  We will discuss how Scotch is made and what influences the differences in flavor. This is your chance to experience a range of fine whisky malts side by side.  Gourmet cheeses will bring out the nuances of the Scotch, and friend/cheese expert Jenn Terraciano will discuss the origin and production of the cheeses as well. Whether you are a scotch connoisseur, a cheese lover, or just want to experience something new, come spend a fun evening with us. This party is for adults, and is priced per person.</p>'
+p.description='<p>Join us for an evening of fine whisky tasting and cheese pairing.</p><p>We will taste selections from all over Scotland - Lowlands, Highlands, Speyside, and Islay malts.  We will discuss how Scotch is made and what influences the differences in flavor.</p><p>This is your chance to experience a range of fine whisky malts side by side.  Gourmet cheeses will bring out the nuances of the Scotch, and friend/cheese expert Jenn Terraciano will discuss the origin and production of the cheeses as well.</p><p>Whether you are a scotch connoisseur, a cheese lover, or just want to experience something new, come spend a fun evening with us.</p><p><em>This party is for adults, and is priced per person.</em></p>'
 p.price='$40/adult'
 p.date='Saturday, January 31, 2015, 7-10pm'
 p.location='The Gudiksen Home, Piedmont'
@@ -72,7 +72,7 @@ p.save
 
 p=Party.find_or_create(title:'A Lesson in Massage and Babysitting')
 p.donor='King Family (Gavin)'
-p.description='<p>Learn some easy and fun techniques for massaging your partner. You and your partner will get to practice on each other, receiving instruction from Heather King, who has 22 years of practice in the art.  Her husband Barry will watch your children with a movie (Frozen or The Boxcar Children). This item is priced per couple, and includes babysitting. </p>'
+p.description='<p>Learn some easy and fun techniques for massaging your partner.</p><p>You and your partner will get to practice on each other, receiving instruction from Heather King, who has 22 years of practice in the art.  Her husband Barry will watch your children with a movie (Frozen or The Boxcar Children).</p><p><em>This item is priced per couple, and includes babysitting.</em></p>'
 p.price='$40/couple'
 p.date='Saturday, February 7, 2015 5-6:30pm'
 p.location='The King Home, Oakland'
@@ -82,7 +82,7 @@ p.save
 
 p=Party.find_or_create(title:'Let\'s Play Mah Jongg')
 p.donor='Horner-Lemley Family (Rita)'
-p.description='<p>Originated in China during the time of Confucius, Mah Jongg is a rummy-like game played with tiles rather than cards. Your hosts Amy Lemley and Justin Horner will kick off the evening with a Mah Jongg-inspired cocktail of ginger-infused liqueur and a short tutorial on the rules of this fun and easy game. Then we\'ll break out into groups of four to play Mah Jongg and take a break mid-evening for a tasty Chinese dinner. This party is for adults, and is priced per person.</p>'
+p.description='<p>Originated in China during the time of Confucius, Mah Jongg is a rummy-like game played with tiles rather than cards.</p><p>Your hosts Amy Lemley and Justin Horner will kick off the evening with a Mah Jongg-inspired cocktail of ginger-infused liqueur and a short tutorial on the rules of this fun and easy game. Then we\'ll break out into groups of four to play Mah Jongg and take a break mid-evening for a tasty Chinese dinner.</p><p><em>This party is for adults, and is priced per person.</em></p>'
 p.price='$40/adult'
 p.date='Saturday, February 28, 6:30-10:30pm'
 p.location='The Horner-Lemley Home, Oakland'
@@ -92,7 +92,7 @@ p.save
 
 p=Party.find_or_create(title:'Craft Cocktail Party')
 p.donor='Warner/Okamoto Families (Vera/Lucy)'
-p.description='<p>Like cocktails, appetizers, and sunsets? Come join the Okamoto and Warner families and enjoy flights of homemade and specially designed cocktails, crafted with all sorts of wacky ingredients: black tea, pineapple, bacon, potatoes, mint, cherries, green tea, lemon, and lots and lots of booze! Of course, we\'ll also have tasty vittles on hand, and a spectacular view of the sun setting over the Bay. Chauffeuring to and from the party in a luxury mini-van is available. This party is for adults, and is priced per person.</p>'
+p.description='<p>Like cocktails, appetizers, and sunsets?</p><p>Come join the Okamoto and Warner families and enjoy flights of homemade and specially designed cocktails, crafted with all sorts of wacky ingredients: black tea, pineapple, bacon, potatoes, mint, cherries, green tea, lemon, and lots and lots of booze! Of course, we\'ll also have tasty vittles on hand, and a spectacular view of the sun setting over the Bay. Chauffeuring to and from the party in a luxury mini-van is available.</p><p><em>This party is for adults, and is priced per person.</em></p>'
 p.price='$40/adult'
 p.date='Saturday March 28, 2015, 7-10pm'
 p.location='The Warner Home, Oakland'
@@ -102,7 +102,7 @@ p.save
 
 p=Party.find_or_create(title:'Hike and Brunch Party')
 p.donor='Allas-Johnson/Baughman-Baldwin Families (Ozzy/Emma Sue)'
-p.description='<p>Take the whole family on a guided hike in Oakland\'s own beautiful Joaquin Miller Park! We\'ll start at the ranger station parking lot and walk along paved paths to one (or more) of several historical monuments hand-built by the famous poet himself! Once there, you\'ll be treated to a brunch cooked onsite by Tom and Adam. The whole trip will be about 1.5 miles - perfect for the little ones! Food and supplies will be provided. Rain or shine! This party is priced per family.</p>'
+p.description='<p>Take the whole family on a guided hike in Oakland\'s own beautiful Joaquin Miller Park!</p><p>We\'ll start at the ranger station parking lot and walk along paved paths to one (or more) of several historical monuments hand-built by the famous poet himself! Once there, you\'ll be treated to a brunch cooked onsite by Tom and Adam.</p><p>The whole trip will be about 1.5 miles - perfect for the little ones! Food and supplies will be provided.  Rain or shine!</p><p><em>This party is priced per family.</em></p>'
 p.price='$50/family'
 p.date='Sunday, March 22, 2015, 10am – approx. 1pm'
 p.location='Joaquin Miller Community Center, 3594 Sanborn Road (at Joaquin Miller Road) '
@@ -112,7 +112,7 @@ p.save
 
 p=Party.find_or_create(title:'Beer and BBQ Party')
 p.donor='Brescher-Romero Family (Alessa)'
-p.description='<p>Like beer?  Ever wanted to learn how it\'s made?  Well, come hang out with the Brescher-Romero family and learn all about brewing beer.  You will take part in all the steps of brewing your own beer; come grind the grain, make the mash and wort, pitch the yeast, learn about specific gravity and what the heck I\'m talking about.  Yes, beer making is a science, and yes, the husband is a smarty-pants and can tell you all about it; but it\'s fun too. This is a family affair, the kids can play while the adults "learn", but everyone gets to enjoy it! We promise, you will leave with your head and belly full.  Now, what fun is making beer unless you get to drink some, too?  So, while we make beer you will also get to drink our homebrew, have some snacks, and enjoy grilled BBQ lunch (w/ vegetarian options). You will also receive your very own growler and have the choice of taking home beer we\'ll be serving from our kegs, or taking a rain check for a fill later on. It\'s up to you. This party is priced per family.</p>'
+p.description='<p>Like beer?  Ever wanted to learn how it\'s made? Well, come hang out with the Brescher-Romero family and learn all about brewing beer.</p><p>You will take part in all the steps of brewing your own beer; come grind the grain, make the mash and wort, pitch the yeast, learn about specific gravity and what the heck I\'m talking about. Yes, beer making is a science, and yes, the husband is a smarty-pants and can tell you all about it; but it\'s fun too.</p><p>This is a family affair, the kids can play while the adults "learn", but everyone gets to enjoy it! We promise, you will leave with your head and belly full.  Now, what fun is making beer unless you get to drink some, too?  So, while we make beer you will also get to drink our homebrew, have some snacks, and enjoy grilled BBQ lunch (w/ vegetarian options).</p><p>You will also receive your very own growler and have the choice of taking home beer we\'ll be serving from our kegs, or taking a rain check for a fill later on. It\'s up to you.</p><p><em>This party is priced per family.</em></p>'
 p.price='$60/family'
 p.date='Saturday, April 25, 2015, 11:30-4pm'
 p.location='The Brescher-Romero Home, Oakland'
@@ -122,7 +122,7 @@ p.save
 
 p=Party.find_or_create(title: 'Make Your Own Pizza Party')
 p.donor='Mandzik-Kahn Family (Zora)'
-p.description='<p>Roll, toss, and top your own personal pizza and fire it in an outdoor brick oven. Adults and kids will make yummy vegetarian pizzas (including the infamous dessert pizza!) Join Zora and her family and relax around their Koi pond eating pizza you made yourself while the kids play happily in the courtyard. Event will be rescheduled in event of rain. This party is priced per family. </p>'
+p.description='<p>Roll, toss, and top your own personal pizza and fire it in an outdoor brick oven.</p><p>Adults and kids will make yummy vegetarian pizzas (including the infamous dessert pizza!) Join Zora and her family and relax around their Koi pond eating pizza you made yourself while the kids play happily in the courtyard.</p><p>Event will be rescheduled in event of rain.</p><p><em>This party is priced per family.</em></p>'
 p.price='$50/family'
 p.date='Saturday, May 16, 2015, 3-7pm'
 p.location='The Mandzik-Kahn Home, Oakland'
@@ -132,7 +132,7 @@ p.save
 
 p=Party.find_or_create(title:'Day of Swimming')
 p.donor='Latronica/Kemp/Armstead Families (Jeffrey/Carly/Elle)'
-p.description='<p>The Latronica, Kemp, and Armstead families invite your family to come out and enjoy a day of swimming, BBQ, and friends.  Please join us at our family pool, "The Hills" for a full day of swimming and lunch. This party is priced per family.</p>'
+p.description='<p>The Latronica, Kemp, and Armstead families invite your family to come out and enjoy a day of swimming, BBQ, and friends.</p><p>Please join us at our family pool, "The Hills" for a full day of swimming and lunch.</p><p><em>This party is priced per family.</em></p>'
 p.price='$60/family'
 p.date='Saturday June 6, 2015, starting at 10am'
 p.location='The Hills Swim and Tennis Club, 2400 Manzanita Drive, Oakland'
@@ -142,7 +142,7 @@ p.save
 
 p=Party.find_or_create(title:'Family Portrait Session')
 p.donor='Wrubel-Wilson/Khouri Families (Naomi/Connor)'
-p.description='<p>Remember just how hard it was to find a recent family portrait for the family bulletin board at the beginning of the school year?  Let\'s take care of that! We\'ll have two skilled photographers to make you say cheese: Sequoia photographer and Naomi\'s Mommy, Hayley Wrubel, and avid photographer and Connor\'s Daddy, Kenneth Khouri.  Hayley and Kenneth will each give you a 10 minute family portrait sitting, for a total of 20 minutes. The lovely foliage at Lake Temescal will be your background.  Your photos will be uploaded to smugmug for you to print or order. This item is priced per family.</p>'
+p.description='<p>Remember just how hard it was to find a recent family portrait for the family bulletin board at the beginning of the school year?</p><p>Let\'s take care of that! We\'ll have two skilled photographers to make you say cheese: Sequoia photographer and Naomi\'s Mommy, Hayley Wrubel, and avid photographer and Connor\'s Daddy, Kenneth Khouri.  Hayley and Kenneth will each give you a 10 minute family portrait sitting, for a total of 20 minutes.</p><p>The lovely foliage at Lake Temescal will be your background.  Your photos will be uploaded to smugmug for you to print or order.</p><p><em>This item is priced per family.</em></p>'
 p.price='$30/family'
 p.location='Lake Temescal, South Entrance, 6500 Broadway Terrace, Oakland, 94618. '
 p.date='Saturday May 2, 2015; a 20 minute timeslot between 10am and Noon.'
@@ -152,42 +152,42 @@ p.save
 
 g=Good.find_or_create(title:'Professional Bicycle Tune-Up')
 g.donor='Allas-Johnson Family (Ozzy)'
-g.description='Adam Allas has 4 years of experience as a professional bicycle mechanic, and has been building and maintaining his own bikes for nearly two decades. This tune-up would cost $85 or more if performed at a bike shop.  Tune-up will include: Brake adjustment Derailer adjustment Bearing adjustments Frame wipe-down Drivetrain cleaning and lubrication Cable lubrication Wheel truing Inspection/adjustment of mounted accessories Comprehensive safety inspection and test ride Cost of any needed replacement parts (inner tubes, cables, brake pads, etc...) will be owner\'s responsibility.  Note: This tune-up is intended as a normal maintenance service for bicycles in decent condition and repair. Bikes in extremely poor condition with significant mechanical issues are not appropriate for this service. See Adam with any questions.  This item is priced per bicycle.'
+g.description='<p>Get your wheels in shape!</p><p>Adam Allas has 4 years of experience as a professional bicycle mechanic and has been building and maintaining his own bikes for nearly two decades. This tune-up would cost $85 or more if performed at a bike shop.</p><p>Tune-up will include<ul><li>Brake adjustment</li><li>Derailer adjustment</li><li>Bearing adjustments</li><li>Frame wipe-down</li><li>Drivetrain cleaning and lubrication</li><li>Cable lubrication</li><li>Wheel truing</li><li>Inspection/adjustment of mounted accessories</li><li>Comprehensive safety inspection and test ride</li></ul><p>Cost of any needed replacement parts (inner tubes, cables, brake pads, etc...) will be owner\'s responsibility.</p><p>Note: This tune-up is intended as a normal maintenance service for bicycles in decent condition and repair. Bikes in extremely poor condition with significant mechanical issues are not appropriate for this service.</p><p>See Adam with any questions.</p><p><em>This item is priced per bicycle.</em></p>'
 g.price='$45'
 g.slots=4
 g.save
 
 g=Good.find_or_create(title:'Recipe Book')
 g.donor='Catalino Family (Lucia)'
-g.description='The Sequoia Family Cookbook is filled with a bounty of recipes that are made with love by our own Sequoia Families. From Breakfast Oatmeal Cupcakes to Cuban "Ropa Vieja", you\'re sure to find a number of dishes for the whole family to enjoy. There is no doubt that you and your children will treasure this little handmade memento for years to come. '
+g.description='<p>The Sequoia Family Cookbook is filled with a bounty of recipes that are made with love by our own Sequoia Families.</p><p>From Breakfast Oatmeal Cupcakes to Cuban "Ropa Vieja", you\'re sure to find a number of dishes for the whole family to enjoy. There is no doubt that you and your children will treasure this little handmade memento for years to come.</p>'
 g.price='$15'
 g.slots=29
 g.save
 
 g=Good.find_or_create(title: 'Sequoia\'s Greatest Hits CD')
 g.donor='Teacher Lucy'
-g.description='A 2-CD set collection of favorite songs from our very own Sequoia kids. Sure to become a top choice for car trips and dance parties!'
+g.description='<p>A 2-CD set collection of favorite songs from our very own Sequoia kids.</p><p>Sure to become a top choice for car trips and dance parties!</p>'
 g.price='$15'
 g.slots=29
 g.save
 
 g=Good.find_or_create(title: 'Personalized Magnet')
 g.donor='Johnson-Torgersen Family (Ava)'
-g.description='Give a photo or a piece of your child\'s artwork to Andrea Johnson, and she will turn it into a magnet so that you can keep the memory forever. These 4x4 magnets are ceramic tile, and sealed to last. Great for you and your child as a keepsake.  Great as a holiday gift for grandparents!'
+g.description='<p>Magnify your child\'s art!</p><p>Give a photo or a piece of your child\'s artwork to Andrea Johnson, and she will turn it into a magnet so that you can keep the memory forever. These 4x4 magnets are ceramic tile, and sealed to last.</p><p>Great for you and your child as a keepsake.  Great as a holiday gift for grandparents!</p>'
 g.price='$10'
 g.slots=30
 g.save
 
 g=Good.find_or_create(title:'Personalized Set of Four Coasters')
 g.donor='Johnson-Torgersen Family (Ava)'
-g.description='Pick four of your favorite pieces of your child\'s artwork or photos, and Andrea Johnson will turn it into a set of 4 coasters. These 4x4 coasters are ceramic tile, and sealed to last. Useful, beautiful, and a wonderful way to remember this special time in your child\'s life.'
+g.description='<p>The coasters with the "most-ers".</p><p>Pick four of your favorite pieces of your child\'s artwork or photos, and Andrea Johnson will turn it into a set of 4 coasters. These 4x4 coasters are ceramic tile, and sealed to last. Useful, beautiful, and a wonderful way to remember this special time in your child\'s life.</p>'
 g.price='$30'
 g.slots=3
 g.save
 
 g=Good.find_or_create(title:'Personalized Mini-Album')
 g.donor='Johnson-Torgersen Family (Ava)'
-g.description='A custom 8x8 album to capture your child\'s year at Sequoia.  Andrea Johnson will take photos of your child from the smugmug website and your child\'s artwork that you have collected, and put them together in a completely handmade and 100% personalized album for you to treasure.'
+g.description='<p>A custom 8x8 album to capture your child\'s year at Sequoia.</p><p>Andrea Johnson will take photos of your child from the smugmug website and your child\'s artwork that you have collected, and put them together in a completely handmade and 100% personalized album for you to treasure.</p>'
 g.price='$50' 
 g.slots=2
 g.save
