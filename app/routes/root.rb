cc47eb5acbb08c9487fd62eef310cf996c866543
Patrick Farrell <pfarrell@prosper.com>
2014-11-11 01:51:05 -0800

    some basic styling and form posts

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -4,10 +4,30 @@ class App < Sinatra::Application
   end
 
   get "/party/:id" do
-    haml :party, locals: {party: Party[params[:id]]}
+    haml :party, locals: {party: Party[params[:id].to_i]}
+  end
+
+  post "/party/:id" do
+    p = Party[params[:id].to_i]
+    s = Signup.new
+    s.email = params[:email]
+    s.name = params[:name]
+    s.party = p
+    s.save
+    haml :party, locals: {party: Party[params[:id].to_i]}
   end
 
   get "/good/:id" do
-    haml :good, locals: {good: Good[params[:id]]}
+    haml :good, locals: {good: Good[params[:id].to_i]}
+  end
+
+  post "/good/:id" do
+    g = Good[params[:id].to_i]
+    s = Signup.new
+    s.email = params[:email]
+    s.name = params[:name]
+    s.good = g
+    s.save
+    haml :good, locals: {good: Good[params[:id].to_i]}
   end
 end
