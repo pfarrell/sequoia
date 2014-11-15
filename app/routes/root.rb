e6b52ad7aec4c940a9e58ce9d0a2ee308436ea74
Patrick Farrell <pfarrell@prosper.com>
2014-11-14 20:25:50 -0800

    back online

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -1,29 +1,51 @@
 class App < Sinatra::Application
   get "/" do
-    haml :index#, locals: {parties: Party.all, goods: Good.all, photos: Photo.all} 
+    haml :index, locals: {parties: Party.all, goods: Good.all, photos: Photo.all} 
   end
 
   get "/party/:id" do
-    redirect '/'
+    haml :party, locals: {party: Party[params[:id].to_i]}
   end
 
   post "/party/:id" do
-    redirect '/'
+    p = Party[params[:id].to_i]
+    s = Signup.new
+    s.email = params[:email]
+    s.name = params[:name]
+    s.party = p
+    s.save
+    haml :party, locals: {party: Party[params[:id].to_i]}
   end
 
   get "/good/:id" do
-    redirect '/'
+    haml :good, locals: {good: Good[params[:id].to_i]}
   end
 
   post "/good/:id" do
-    redirect '/'
+    g = Good[params[:id].to_i]
+    s = Signup.new
+    s.email = params[:email]
+    s.name = params[:name]
+    s.good = g
+    s.save
+    haml :good, locals: {good: Good[params[:id].to_i]}
   end
 
   get "/photo" do
-    redirect '/'
+    haml :photo, locals: {photos: Photo.all}
   end
 
   post "/photo/:id" do
-    redirect '/'
+    p = Photo.where(slot: params[:id].to_i).first
+    puts "hello #{p.inspect}"
+    if(p.nil?)
+      p = Photo.new
+      p.slot = params[:id].to_i
+      p.name = params[:name]
+      p.email = params[:email]
+      p.save
+    end
+    
+    haml :photo, locals: {photos: Photo.all}
   end
 end
