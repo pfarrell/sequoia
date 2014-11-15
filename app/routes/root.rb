f9520e23c42f5e0d4d2fe3869fdcc7e31c98e170
Patrick Farrell <pfarrell@prosper.com>
2014-11-14 20:29:13 -0800

    disabled posts

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -8,13 +8,7 @@ class App < Sinatra::Application
   end
 
   post "/party/:id" do
-    p = Party[params[:id].to_i]
-    s = Signup.new
-    s.email = params[:email]
-    s.name = params[:name]
-    s.party = p
-    s.save
-    haml :party, locals: {party: Party[params[:id].to_i]}
+    redirect(url_for "/")
   end
 
   get "/good/:id" do
@@ -22,13 +16,7 @@ class App < Sinatra::Application
   end
 
   post "/good/:id" do
-    g = Good[params[:id].to_i]
-    s = Signup.new
-    s.email = params[:email]
-    s.name = params[:name]
-    s.good = g
-    s.save
-    haml :good, locals: {good: Good[params[:id].to_i]}
+    redirect(url_for "/")
   end
 
   get "/photo" do
@@ -36,16 +24,6 @@ class App < Sinatra::Application
   end
 
   post "/photo/:id" do
-    p = Photo.where(slot: params[:id].to_i).first
-    puts "hello #{p.inspect}"
-    if(p.nil?)
-      p = Photo.new
-      p.slot = params[:id].to_i
-      p.name = params[:name]
-      p.email = params[:email]
-      p.save
-    end
-    
-    haml :photo, locals: {photos: Photo.all}
+    redirect(url_for "/")
   end
 end
